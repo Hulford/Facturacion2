2b91c3b28aed5f4364e59bf80e7d04459d209078
Hulford <moraleshulford@gmail.com>
2022-10-09 18:26:04 -0500

    Commit7
    Leccion7Herencia2

--- facturacion/src/com/tuempresa/facturacion/modelo/Factura.java
+++ facturacion/src/com/tuempresa/facturacion/modelo/Factura.java
@@ -1,46 +1,14 @@
 package com.tuempresa.facturacion.modelo;
-import java.time.*;
+
 import java.util.*;
 
 import javax.persistence.*;
 
-import org.openxava.annotations.*;
-import org.openxava.calculators.*;
-
 import lombok.*;
+
 @Entity @Getter @Setter
-@View(members =
-"anyo, numero, fecha;"+
-"cliente;"+
-		"detalles;"+
-"observaciones")
-public class Factura extends Identificable{
-	
-	 @Column(length=4)
-	 @DefaultValueCalculator(CurrentYearCalculator.class)
-	 int anyo;
-	 @Column(length=6)
-	 int numero;
-	 
-	 @Required
-	@DefaultValueCalculator(CurrentLocalDateCalculator.class)
-	 LocalDate fecha;
-	 
-	 @ManyToOne(fetch=FetchType.LAZY, optional=false)
-	 @ReferenceView("Simple")
-	 Cliente cliente;
-	 
-	@ElementCollection
-	@ListProperties("producto.numero, producto.descripcion, cantidad")
-	 Collection<Detalle> detalles;
-	 
-	 @TextArea
-	 String observaciones;
-	 
-	 
-	
-	
-	
-	
-	
+public class Factura extends DocumentoComercial{
+
+	@OneToMany(mappedBy = "factura" )
+	Collection<Pedido> pedidos;
 }
