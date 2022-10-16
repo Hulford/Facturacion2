1fa5d9077f8e2e42112894bf3bcce40fe286fa0f
Hulford <moraleshulford@gmail.com>
2022-10-16 09:38:30 -0500

    Commit12
    Leccion12

--- facturacion/src/com/tuempresa/facturacion/modelo/DocumentoComercial.java
+++ facturacion/src/com/tuempresa/facturacion/modelo/DocumentoComercial.java
@@ -1,8 +1,10 @@
 package com.tuempresa.facturacion.modelo;
+import java.math.*;
 import java.time.*;
 import java.util.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 import org.openxava.annotations.*;
 import org.openxava.calculators.*;
@@ -29,16 +31,34 @@ abstract public class DocumentoComercial extends Identificable{
 	@DefaultValueCalculator(CurrentLocalDateCalculator.class)
 	 LocalDate fecha;
 	 
+	 @Stereotype("MENU")
+	 String observaciones;
+	 
 	 @ManyToOne(fetch=FetchType.LAZY, optional=false)
 	 @ReferenceView("Simple")
 	 Cliente cliente;
 	 
 	@ElementCollection
-	@ListProperties("producto.numero, producto.descripcion, cantidad, precioPorUnidad, importe")
-	 Collection<Detalle> detalles;
+	@ListProperties("producto.numero, producto.descripcion, cantidad, precioPorUnidad, " +
+			"importe+[" + 
+			"documentoComercial.porcentajeIVA," +
+			"documentoComercial.iva," +
+			"documentoComercial.importeTotal" +
+			"]")
+	 private Collection<Detalle> detalles;
+	
+	@Digits(integer=2, fraction=0) 
+	BigDecimal porcentajeIVA;
+	@ReadOnly
+	@Money
+	@Calculation("sum(detalles.importe) * porcentajeIVA / 100")
+	BigDecimal iva;
+	@ReadOnly
+	@Money
+	@Calculation("sum(detalles.importe) + iva") 
+	BigDecimal importeTotal; 
+	
 	 
-	 @TextArea
-	 String observaciones;
 	 
 	 
 	
